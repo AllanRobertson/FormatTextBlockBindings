c969bba80f445481fd34396a1cb89f539cb480c8
Allan Robertson <Allan.Robertson@PerkinElmer.com>
2018-02-26 19:23:35 -0500

    Code developed using Visual Studio 2017 and from info from https://stackoverflow.com/questions/5582893/wpf-generate-textblock-inlines

---
+++ TextBlockFormating/MainWindowViewModel.cs
@@ -0,0 +1,47 @@
+﻿
+using System;
+using System.Collections.Generic;
+
+namespace TextBlockFormating
+{
+    public class MainWindowViewModel : ViewModelBase
+    {
+
+        private List<string> _stringList = new List<string> { "The", "Quick", "Brown", "Fox", "Ran", "Over", "The", "Lazy", "Dog" };
+
+        int _index = 0;
+
+        public MainWindowViewModel()
+        {
+            StepCommand = new RelayCommand(OnStepCommand);
+        }
+
+
+        private string _formattedText;
+
+        public string FormattedText
+        {
+            get { return _formattedText; }
+            set
+            {
+                if (value != _formattedText)
+                {
+                    _formattedText = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        public RelayCommand StepCommand { get; private set; }
+
+        private void OnStepCommand()
+        {
+            if (_index == _stringList.Count)
+            {
+                _index = 0;
+            }
+            FormattedText = string.Format("<Bold>The next string is</Bold> <Span FontStyle=\"Italic\">{0}</Span>", _stringList[_index++]);
+        }
+
+    }
+}
---
+++ TextBlockFormating/ViewModelBase.cs
@@ -0,0 +1,52 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace TextBlockFormating
+{
+    public abstract class ViewModelBase : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged(PropertyChangedEventArgs eventArgs)
+        {
+            PropertyChangedEventHandler propertyChanged = PropertyChanged;
+            if (propertyChanged != null)
+            {
+                propertyChanged(this, eventArgs);
+            }
+        }
+
+        public void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            OnPropertyChanged(new PropertyChangedEventArgs(propertyName));
+        }
+
+        /// <summary>
+        /// Helper Method to raise INotifyProperty change.
+        /// </summary>
+        protected void RaiseAndSetIfChanged(ref string field, string value, [CallerMemberName] string propertyName = null)
+        {
+
+            if (field == value) return;
+
+            if (field == null && string.IsNullOrEmpty(value)) return;
+            if (string.IsNullOrEmpty(field) && value == null ) return;
+
+            field = value;
+
+            OnPropertyChanged(new PropertyChangedEventArgs(propertyName));
+        }
+
+        /// <summary>
+        /// Helper Method to raise INotifyProperty change.
+        /// </summary>
+        protected void RaiseAndSetIfChanged(ref bool field, bool value, [CallerMemberName] string propertyName = null)
+        {
+            if (field == value) return;
+            field = value;
+
+            OnPropertyChanged(new PropertyChangedEventArgs(propertyName));
+        }
+
+    }
+}
